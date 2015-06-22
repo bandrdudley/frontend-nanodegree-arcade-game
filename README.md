516a7c7ce6d8d0a6c3c1282f0f1da495b5d34b29
Brian Dudley (bd7946) <bandrdudley@gmail.com>
2015-06-22 19:47:57 -0400

    Frogger Code Functionality
    Added bugs and character

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
 frontend-nanodegree-arcade-game
 ===============================
 
-Students should use this rubric: https://www.udacity.com/course/viewer#!/c-ud015/l-3072058665/m-3072588797
+Steps Required to Run and Play the Application:
 
-for self-checking their submission.
+* Use the Arrow Keys to move your character on the Game Board
+	Left arrow: Moves your character one square to the left
+	Right arrow: Moves your character one square to the right
+	Up arrow: Moves your character one square up
+	Down arrow: Moves your character one square down
+	
+	Please note: Your character cannot fall off of the Game Board
+				  And it cannot pass from one side and reappear on	
+				  the other side.
+	
+* Game Strategy - Avoid the Bugs or Enemies
+		The Bugs travel across three Lanes in a random pattern
+		If your character makes contact with a Bug, you lose and your
+		character will be reset back to its original position on	
+		the Game Board
+		
+* How to Win
+	Avoid the Bugs and Have your Character Reach the Water: You Win!
+	
+Have fun!	
--- js/app.js
+++ js/app.js
@@ -1,20 +1,47 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, v) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
+	this.x = x;
+	this.y = y;
+	this.v = v;
+	this.w = 101;
+	this.h = 83;
     this.sprite = 'images/enemy-bug.png';
 }
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
+	var rectWidth = 101;
+	var rectHeight = 50;
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+	if(this.x > 505) {
+		this.x = -100;
+		var lane = Math.floor(Math.random() * 3);
+		var loc = [60, 140, 220];	
+		this.y = loc[lane];
+	}
+	this.x = this.x + this.v * dt;
+	
+	//Handles collision with the Player
+	//when two entities occupy the same space, for instance when your character should die
+	//
+	if (this.x < player.x + player.w &&
+	   this.x + this.w > player.x &&
+	   this.y < player.y + player.h &&
+	   this.h + this.y > player.y) {
+		// collision detected!
+		player.reset();
+	}
+
 }
+
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
@@ -24,13 +51,72 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+	this.x = 200; 
+	this.y = 375;
+	this.dx = 100;
+	this.dy = 80;
+	this.w = 101;
+	this.h = 83;
+	this.sprite = 'images/char-boy.png';
+}
 
+Player.prototype.update = function(dt) {
+
+}
+
+Player.prototype.render = function() {
+	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Player.prototype.handleInput = function(allowedKeys) {
+		switch(allowedKeys) {
+			case "right":
+				this.x += this.dx;
+				break;
+			case "left":
+				this.x -= this.dx;
+				break;
+			case "up":
+				this.y -= this.dy;
+				break;
+			case "down":
+				this.y += this.dy;
+				break;				
+			default:
+				console.log("Undefined allowedKeys");
+		}
+	
+		if(this.y < 0) { // reached water
+			player.reset();
+		}
+		if(this.x < 0) {
+			this.x = 0;
+		}
+		if(this.x > 400) {
+			this.x = 400;
+		}
+		if(this.y > 400) {
+			this.y = 375;
+		}
+}	
+
+Player.prototype.reset = function() {
+	this.x = 200; 
+	this.y = 375;
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+var allEnemies = [];
+var enemyOne = new Enemy(0,60,60);
+var enemyTwo = new Enemy(0,140,30);
+var enemyThree = new Enemy(0,220,120);
+allEnemies.push(enemyOne);
+allEnemies.push(enemyTwo);
+allEnemies.push(enemyThree);
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
