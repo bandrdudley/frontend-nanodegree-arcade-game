5a6009403b31f5fe634e317408bdef5079660d68
Brian Dudley (bd7946) <bd7946@att.com>
2015-10-02 22:33:50 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,27 @@
+frontend-nanodegree-arcade-game
+===============================
+
+Steps Required to Run and Play the Application:
+
+^ Launch the game by double clicking the index.html file
+
+* Use the Arrow Keys to move your character on the Game Board
+	Left arrow: Moves your character one square to the left
+	Right arrow: Moves your character one square to the right
+	Up arrow: Moves your character one square up
+	Down arrow: Moves your character one square down
+	
+	Please note: Your character cannot fall off of the Game Board
+				  And it cannot pass from one side and reappear on	
+				  the other side.
+	
+* Game Strategy - Avoid the Bugs or Enemies
+		The Bugs travel across three Lanes in a random pattern
+		If your character makes contact with a Bug, you lose and your
+		character will be reset back to its original position on	
+		the Game Board
+		
+* How to Win
+	Avoid the Bugs and Have your Character Reach the Water: You Win!
+	
+Have fun!	
---
+++ js/app.js
@@ -0,0 +1,129 @@
+// Enemies our player must avoid
+var Enemy = function(x, y, v) {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+	this.x = x;
+	this.y = y;
+	this.v = v;
+	this.w = 70;
+	this.h = 70;
+    this.sprite = 'images/enemy-bug.png';
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+	if(this.x > 505) {
+		this.x = -100;
+		var lane = Math.floor(Math.random() * 3);
+		var loc = [60, 140, 220];	
+		this.y = loc[lane];
+	}
+	this.x = this.x + this.v * dt;
+	
+	//Handles collision with the Player
+	//when two entities occupy the same space, for instance when your character should die
+	//
+	if (this.x < player.x + player.w &&
+	   this.x + this.w > player.x &&
+	   this.y < player.y + player.h &&
+	   this.h + this.y > player.y) {
+		// collision detected!
+		player.reset();
+	}
+
+}
+
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+var Player = function() {
+	this.x = 200; 
+	this.y = 375;
+	this.dx = 100;
+	this.dy = 80;
+	this.w = 70;
+	this.h = 70;
+	this.sprite = 'images/char-boy.png';
+};
+
+Player.prototype.update = function(dt) {
+
+}
+
+Player.prototype.render = function() {
+	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Player.prototype.handleInput = function(allowedKeys) {
+		switch(allowedKeys) {
+			case "right":
+				this.x += this.dx;
+				break;
+			case "left":
+				this.x -= this.dx;
+				break;
+			case "up":
+				this.y -= this.dy;
+				break;
+			case "down":
+				this.y += this.dy;
+				break;				
+			default:
+				console.log("Undefined allowedKeys");
+		}
+	
+		if(this.y < 0) { // reached water
+			player.reset();
+		}
+		if(this.x < 0) {
+			this.x = 0;
+		}
+		if(this.x > 400) {
+			this.x = 400;
+		}
+		if(this.y > 400) {
+			this.y = 375;
+		}
+}
+
+Player.prototype.reset = function() {
+	this.x = 200; 
+	this.y = 375;
+}
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+var allEnemies = [];
+
+for(var i=0; i< 3; i++) {
+	allEnemies.push(new Enemy(0, 60+80*i,30+30*i));
+}
+
+var player = new Player();
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
